7422fb2660c30af9d692d7be289b0a7733ca0a15
janaitis <133566578+janaitis@users.noreply.github.com>
2023-05-14 12:33:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.unser.first_name} ${tg.initDataUnsafe.unser.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 usercard.appendChild(p);
 
 
